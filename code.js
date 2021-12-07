d5d1ac0affcf18bcae842ff350f39a27b02a4e1a
Abigail Trapp <abbsthepandahead@gmail.com>
2021-12-06 23:33:35 -0700

    new test commit

--- code.js
+++ code.js
@@ -7,13 +7,14 @@ function miniMaxSum(arr) {
     let maxFilter = arr.filter(arr => arr !== max).reduce((acc,n)=>acc+n)
     let minFilter = arr.filter(arr => arr !== min).reduce((acc,n)=>acc+n)
     
-    let output = (`${maxFilter} ${minFilter}`)
+    let output = (`${maxFilter},${minFilter}`)
     return output
     
 }
 //document.write(`[1,3,4,7,8] = ${miniMaxSum([1,3,4,7,8])}`)
 let heading = document.querySelector("h1")
 let results = document.querySelector("div")
-results.innerHTML = `[1,3,4,7,8] = ${miniMaxSum([1,3,4,7,8])}`
+results.innerHTML = `[1,3,4,7,8] = ${miniMaxSum([1,3,4,7,8])} 
+[2,4,6,10,15] = ${miniMaxSum([2,4,6,10,15])}`
 heading.append(results)
 
